1ab2ba71df5fa4076307baa58d3a8eb0ffaacbb6
MMitchell <130703972+Michem04@users.noreply.github.com>
2023-04-20 16:12:29 -0400

    Update art2music.py

--- art2music.py
+++ art2music.py
@@ -4,3 +4,21 @@
 +#Date 4-20-2023
 +# Version 0.1a
 +#++++++++++++++++++++++++++++
+
+
+#import out libraries
+
+#Keep track of settings
+#handle user input
+
+
+#see if they want to use their own image or a sample image
+#See if we have an image
+
+#Save image into a temp file
+#ask the computer to view the image
+#Grab the hue from HSV
+#Seperate the Hues and convert to frequencey
+#generic song save
+#Make an effected version
+#Save the final song and make a midi
